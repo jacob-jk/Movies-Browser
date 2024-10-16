050490eb3112d3c08ad5234bbcba4c07fee87711
Anna Wójcik <aniawojcik894@gmail.com>
2024-10-16 11:15:32 +0200

    "whisper" color added to theme.js and changed the background color

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,13 @@
 export const theme = {
   color: {
     white: "#FFFFFF",
-    waterloo: "#7E839A",
     mystic: "#E4E6F0",
+    whisper: "#F5F5FA",
     pattensBlue: "#D6E4FF",
     scienceBlue: "#0044CC",
     woodSmoke: "#18181B",
     grey: "#E4E6F0",
+    waterloo: "#7E839A",
     black: "#000000",
   },
   breakpoint: {
