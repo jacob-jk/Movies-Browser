2db9172616496ab6767c25d9f71d3067b4970c4e
Kuba Karnath <jacob_jk@wp.pl>
2024-10-29 13:20:22 +0100

    Wrapper edited

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  max-width: 1368px;
+  max-width: 1416px;
   margin: 56px auto;
+  padding: 0 24px;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     margin: 24px 16px;
+    padding: 0;
   }
 `;
 
